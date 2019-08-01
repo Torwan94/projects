77686c37688f12c76602e34469f04af60546cd5d
Torwan94 <53584992+Torwan94@users.noreply.github.com>
2019-08-02 01:40:10 +0200

    Update losowanie.java

--- losowanie.java
+++ losowanie.java
@@ -18,6 +18,13 @@ public class losowanie {
 		int w5 = los.nextInt(49);
 		int w6 = los.nextInt(49);
 		
+			  System.out.println(c+w1);
+			  System.out.println(c+w2);
+			  System.out.println(c+w3);
+			  System.out.println(c+w4);
+			  System.out.println(c+w5);
+			  System.out.println(c+w6);
+		
 		
 		while(w1==w2 & w1==w3 & w1==w4 & w1==w5 & w1==w6 & w2==w3&w2==w4&w2==w5&w2==w6&w3==w4&w3==w5&w3==w6&w4==w5&w4==w6&w5==w6) {
 			w1=los.nextInt(49);
@@ -27,12 +34,7 @@ public class losowanie {
 			w5=los.nextInt(49);
 			w6=los.nextInt(49);
 			
-			 System.out.println(c+w1);
-			  System.out.println(c+w2);
-			  System.out.println(c+w3);
-			  System.out.println(c+w4);
-			  System.out.println(c+w5);
-			  System.out.println(c+w6);
+
 		} 
 		
 		  
